f65afaa4de8d1b8f61bfc319a6cb6df8d2a6d6cf
Thomas Ballinger <thomasballinger@gmail.com>
2015-04-18 17:31:25 -0400

    two lisp eval techniques

--- lisp.py
+++ lisp.py
@@ -1,7 +1,7 @@
 """Gradual evaluation techniques
 
 ideas:
-    * greenlets
+    * greenlets (skip this, it's boring)
     * nested generators
     * return thunks
 
@@ -9,8 +9,16 @@ ideas:
 TODO:
     parse language that does pygamey stuff
 
+    should be able to swap out portions of AST at any time
+    store last time each function was run
+
 """
+from __future__ import division
+import operator
 import re
+import sys
+from functools import reduce
+
 
 def tokenize(s):
     """
@@ -21,16 +29,128 @@ def tokenize(s):
     """
     return re.findall(r'[()]|[\w\-+/*]+', s)
 
-def parse(s):
+def parse(s, i=0):
     """Lispy syntax -> AST
 
-    #>>> '(+ (thing 1 2) (other 3 4))'
-    
-    #('+', ('thing', 1, 2)
+    >>> parse('(+ (thing 1 2) (other 3 4))')
+    ('+', ('thing', 1, 2), ('other', 3, 4))
 
     """
+    if isinstance(s, str):
+        s = iter(tokenize(s))
+    cur = next(s)
+
+    if cur == '(':
+        form = []
+        while True:
+            f = parse(s)
+            if f == ')':
+                return tuple(form)
+            form.append(f)
+    elif cur == ')':
+        return ')'
+    elif re.match(r'[+-]?\d+', cur):
+        return int(cur)
+    elif re.match('[+-]?\d+\.?\d*', cur):
+        return float(cur)
+    else:
+        return cur
+
+builtins = {
+        '+': lambda *args: sum(args),
+        '-': lambda *args: (reduce(operator.sub, args, 0)
+                            if len(args) == 1
+                            else reduce(operator.sub, args)),
+        '*': lambda *args: reduce(operator.mul, args, 1),
+        '/': lambda x, y: x / y,
+        'display': lambda *args: [sys.stdout.write(
+            ', '.join(repr(x) for x in args) + '\n'), None][-1],
+        }
+
+def eval(ast):
+    """
+
+    >>> eval(('-', 1, 2))
+    -1
+    >>> eval(('+', ('-', 1, 2), ('+', 3, 4)))
+    6
+    >>> eval(('do', ('display', 1), ('display', 2), ('display', 3)))
+    1
+    2
+    3
+    """
+    if isinstance(ast, (int, float)):
+        return ast
+    if ast[0] == 'do':
+        for form in ast[1:]:
+            result = eval(form)
+        return result
+    elif ast[0] in builtins:
+        return builtins[ast[0]](*[eval(f) for f in ast[1:]])
+    elif ast[0] == 'if':
+        assert len(ast) in (3, 4)
+        if eval(ast[1]):
+            return eval(ast[2])
+        elif len(ast) == 4:
+            return eval(ast[3])
 
 
+class Incomplete:
+    """Signal that evaluation is incomplete"""
+    def __repr__(self):
+        return 'Incomplete'
+Incomplete = Incomplete()
+
+
+def eval_with_generator(ast):
+    """
+    >>> eval_with_generator(('+', ('-', 1, 2), ('+', 3, 4)))
+    6
+    """
+    (result, ) = [x for x in eval_generator(ast) if x is not Incomplete]
+    return result
+
+
+def eval_generator(ast):
+    """
+
+    >>> [x for x in eval_generator(['+', 1, 2]) if x is not Incomplete]
+    [3]
+    >>> g = eval_generator(('do', ('display', 1), ('display', 2), ('display', 3)))
+    >>> list(g)
+    1
+    2
+    3
+    [Incomplete, Incomplete, Incomplete, None]
+    """
+    if isinstance(ast, (int, float)):
+        yield ast
+    elif ast[0] == 'do':
+        for form in ast[1:]:
+            g = eval_generator(form)
+            for value in g:
+                yield Incomplete
+        yield value
+    elif ast[0] in builtins:
+        yield builtins[ast[0]](*[eval(f) for f in ast[1:]])
+    elif ast[0] == 'if':
+        assert len(ast) in (3, 4)
+        g = eval_generator(ast[1])
+        for value in g:
+            yield Incomplete
+        if value:
+            yield from eval_generator(ast[2])
+        elif len(ast) == 4:
+            yield from eval_generator(ast[3])
+
+class Evaluation(object):
+    def __init__(self, ast):
+        self.ast = ast
+        self.eval_tree = ast
+        self.path = [ast]
+
+    def eval_step(self):
+        current = self.path[-1]
 
 if __name__ == '__main__':
     import doctest
